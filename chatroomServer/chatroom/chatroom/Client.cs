9ae6e0a9fbce6ad690e84218684435f8210eadb8
Haruho <532685083@qq.com>
2017-09-20 11:40:50 +0800

    Working

---
+++ chatroomServer/chatroom/chatroom/Client.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chatroom
+{
+    class Client
+    {
+        public string name = null;
+
+        private Socket clientSocket;
+
+        private Thread t;
+
+        private byte[] data = new byte[1024];//这个是一个数据容器
+
+        public Client(Socket s, string i)
+
+        {
+
+            clientSocket = s;
+
+            name = i;
+
+            //启动一个线程 处理客户端的数据接收
+
+            t = new Thread(ReceiveMessage);
+
+            t.Start();
+
+        }
+
+        private void ReceiveMessage()
+
+        {
+
+            //一直接收客户端的数据
+
+            while (true)
+
+            {
+
+                //在接收数据之前  判断一下socket连接是否断开
+
+                if (clientSocket.Poll(10, SelectMode.SelectRead))
+
+                {
+
+                    clientSocket.Close();
+
+                    break;//跳出循环 终止线程的执行
+
+                }
+
+                int length = clientSocket.Receive(data);
+
+                string message = Encoding.UTF8.GetString(data, 0, length);
+
+                //接收到数据的时候 要把这个数据 分发到客户端
+
+                //广播这个消息
+
+                Program.BroadcastMessage(message, name);
+
+                Console.WriteLine("收到 " + name + " 的消息:" + message);
+
+            }
+
+        }
+
+        public void SendMessage(string message)
+
+        {
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            clientSocket.Send(data);
+
+        }
+
+        public bool Connected
+
+        {
+
+            get { return clientSocket.Connected; }
+
+        }
+
+    }
+}
+
---
+++ chatroomServer/chatroom/chatroom/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+namespace chatroom
+{
+    class Program
+    {
+        static List<Client> clientList = new List<Client>();
+
+        /// <summary>
+
+        /// 广播消息
+
+        /// </summary>
+
+        /// <param name="message"></param>
+
+        public static void BroadcastMessage(string message, string name)
+
+        {
+
+            var notConnectedList = new List<Client>();
+
+            foreach (var client in clientList)
+
+            {
+
+                if (client.Connected)
+
+                    client.SendMessage(name + " : " + message);
+
+                else
+
+                {
+
+                    notConnectedList.Add(client);
+
+                }
+
+            }
+
+            foreach (var temp in notConnectedList)
+
+            {
+
+                clientList.Remove(temp);
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+
+            string name = null;
+
+            Socket tcpServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//create tcpserver
+
+            tcpServer.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7788));//set ip and port
+
+            tcpServer.Listen(100);//listen port
+
+            Console.WriteLine("server running...");
+
+            while (true)
+
+            {
+
+                i = i + 1;
+
+                if (i == 1) { name = "Tom"; }
+
+                else if (i == 2) { name = "Alice"; }
+
+                else { name = "Other"; }
+
+                Socket clientSocket = tcpServer.Accept();//accept client request
+
+                Console.WriteLine("用户 " + name + " 已连接 !");
+
+                Client client = new Client(clientSocket, name);//把与每个客户端通信的逻辑(收发消息)放到client类里面进行处理
+
+                clientList.Add(client);
+
+            }
+
+        }
+
+    }
+}
